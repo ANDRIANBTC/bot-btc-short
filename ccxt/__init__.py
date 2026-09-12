575fb8b9886c55f4d1386df1de8b9a429ee7dadf
ANDRIANBTC <andriancavalho@gmail.com>
2026-09-11 19:40:55 -0500

    Update __init__.py

--- ccxt/__init__.py
+++ ccxt/__init__.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Evitar importación circular removiendo temporalmente la ruta local
 current_dir = os.path.dirname(__file__)
 if current_dir in sys.path:
     sys.path.remove(current_dir)
@@ -11,10 +10,9 @@ import ccxt as _real_ccxt
 if current_dir not in sys.path:
     sys.path.insert(0, current_dir)
 
-# Re-exportar todo desde la librería real de CCXT
-from ccxt import *
+# Importar desde la librería real instalada
+from _real_ccxt import *
 
-# Sobrescribir la clase binance para asegurar conectividad estable en la nube
 class binance(_real_ccxt.binance):
     def __init__(self, config=None):
         if config is None:
